9b58ef0ff2310d888e7d9a06bd381289dd935e2f
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-10-21 17:53:17 -0400

    Log

--- src/Folklore/EloquentJsonSchema/Support/RelationReducer.php
+++ src/Folklore/EloquentJsonSchema/Support/RelationReducer.php
@@ -35,7 +35,7 @@ abstract class RelationReducer extends Reducer
         // Fallback to query if not found in relations and model doesn't exists
         if ($this->shouldQueryRelation($model, $node, $state, $value)) {
             $relationClass = $this->getRelationClass($model, $node, $state);
-            die($id);
+            dd($id);
             $value = app($relationClass)->find($id)->first();
         }
 
